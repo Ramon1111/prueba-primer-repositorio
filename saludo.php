9bfb260e29dfa89972771f79b72eb9013ef8025a
Ramon1111 <jramoncito7-99@hotmail.com>
2016-05-22 17:40:43 -0500

    Update saludo.php

--- saludo.php
+++ saludo.php
@@ -1,3 +1,4 @@
 <?php
-	echo 'Hola Mundo';
+	$saludo=array('Hola Mundo','Saludos',"Hello, It's me",'Buenas tardes','Buenos dias','Hi','Buenas noches','Hola','¿Qué hay?','¿Cómo estas?');
+	echo $saludo[rand(0,9)];
 ?>
